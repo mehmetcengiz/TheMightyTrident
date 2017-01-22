e55829b8d4f154444c3cf5e77d69c3b051a7d2da
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-01-22 06:23:03 +0300

    Level Musics are added.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts {
+	public class MusicManager : MonoBehaviour {
+
+		public static MusicManager instance = null; // Setup Singleton Pattern So Only One Music Player Object Ever Instantiats
+		public AudioClip startClip;
+		public AudioClip gameClip;
+		public AudioClip lostClip;
+		public AudioClip winClip;
+		private AudioSource _music;
+
+		// Use this for initialization
+		void Awake() {
+			// If a MusicPlayer Already exists
+			if (instance != null && instance != this) {
+				Destroy(gameObject);
+				print("Duplicate music player self destructing");
+			}
+			else {
+				// Don't destroy this game object between scenes (i.e play music all the time between scenes)
+				instance = this; // Claim the instance if it is null i.e. 1st time round
+				GameObject.DontDestroyOnLoad(gameObject);
+				_music = GetComponent<AudioSource>();
+				//Add a DELEGATE to this to get notifications when a scene is load and thus play the appropriate level music
+				SceneManager.sceneLoaded += OnSceneLoaded;
+			}
+		}
+
+		//Load The Appropriate Music Depending On The Scene That Was Loaded
+		void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode) {
+			_music.Stop();
+			if (SceneManager.GetActiveScene().buildIndex == 0) {
+				Debug.Log("MusicPlayer: Loaded Level " + SceneManager.GetActiveScene().buildIndex);
+				_music.clip = startClip;
+				_music.loop = true;
+			}
+			if (SceneManager.GetActiveScene().buildIndex == 1) {
+				Debug.Log("MusicPlayer: Loaded Level " + SceneManager.GetActiveScene().buildIndex);
+				_music.clip = gameClip;
+				_music.loop = true;
+			}
+			if (SceneManager.GetActiveScene().buildIndex == 4) {
+				Debug.Log("MusicPlayer: Loaded Level " + SceneManager.GetActiveScene().buildIndex);
+				_music.clip = lostClip;
+				_music.loop = false;
+			}
+			if (SceneManager.GetActiveScene().buildIndex == 5) {
+				Debug.Log("MusicPlayer: Loaded Level " + SceneManager.GetActiveScene().buildIndex);
+				_music.clip = lostClip;
+				_music.loop = false;
+			}
+
+			_music.Play();
+		}
+	}
+}

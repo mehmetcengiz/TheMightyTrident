58c2bb8ed0401eb2744e1a4585e300b904148c02
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-01-22 04:38:32 +0300

    Game about to finish.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts {
+	public class GameManager : MonoBehaviour {
+
+		public enum GameState {
+			Win,
+			Lost
+		}
+
+		private GameObject _hades;
+		private GameObject _player;
+		private Health _hadesHealth;
+		private Health _playerHealth;
+		private LevelManager _levelManager;
+		
+
+
+		void Start () {
+			_hades = GameObject.Find("Hades");
+			_hadesHealth = _hades.GetComponent<Health>();
+			_player = GameObject.Find("Player");
+			_playerHealth = _player.GetComponent<Health>();
+			_levelManager = FindObjectOfType<LevelManager>();
+		}
+	
+		void Update () {
+			if(_playerHealth.healtCondition == Health.HealtCondition.Death)
+				Invoke("LoadLevel",2);
+			if (_hadesHealth.healtCondition == Health.HealtCondition.Death)
+				Invoke("LoadNextLevel", 2);
+
+		}
+
+		void LoadNextLevel() {
+			_levelManager.LoadNextLevel();
+		}
+
+		void LoadLevel() {
+			_levelManager.LoadLevel("05LostScreen");
+		}
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,23 @@ namespace Assets.Scripts {
 
 		private Animator _playerAnimator;
 		private Rigidbody _rigidbody;
+		private Health myHealth;
 
 		void Start() {
 			_playerAnimator = GetComponent<Animator>();
 			_rigidbody = GetComponent<Rigidbody>();
+			myHealth = GetComponent<Health>();
+		}
+
+		void Update() {
+			if(myHealth.healtCondition == Health.HealtCondition.Death)
+				PerformDeath();
 		}
 
 		void OnTriggerEnter(Collider collider) {
 
 			Enemy myEnemy = collider.transform.GetComponent<Enemy>();
-			Health myHealth = GetComponent<Health>();
-
 			if (myEnemy) {
-				if (myHealth.healtCondition == Health.HealtCondition.Death) {
-					PerformDeath();
-					return;
-				}
 				myHealth.Damaged(myEnemy.enemyDamage);
 			}
 		}
